0915b30828e2ed12507b4ff6bc43a7b11f627341
Mikhail Kuznetsov <mk.mikhail.k@gmail.com>
2024-04-06 08:19:38 +0300

    => saved

--- src/common/actions/common.actions.ts
+++
@@ -1,8 +0,0 @@
-import { createAction } from '@reduxjs/toolkit'
-
-//========================================================================================
-
-export const clearTasksAndTodolists = createAction('common/clear - tasks and todolists')
-
-
-
